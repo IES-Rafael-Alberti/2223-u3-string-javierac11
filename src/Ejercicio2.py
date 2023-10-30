63baae94eca49b21e09f4b7f7a3c594d3ab66729
javierac11 <javieracedocastrillon@gmail.com>
2023-10-30 16:07:53 +0100

    Ejercicio 2

---
+++ src/Ejercicio2.py
@@ -0,0 +1,6 @@
+#Ejercicio 3.0.2¶
+#Dado que fruta es una variable de tipo cadena, 
+# ¿qué significa fruta[:]?
+
+# fruta[:] seria la palabra entera por ejemplo si fruta es "manzana"
+# fruta[:] tambien seria "manzana"
